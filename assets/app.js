00ce6447e246d98424833b319b64146472a3b306
William Sams III <w.sams11@yahoo.com>
2019-10-22 09:51:45 -0700

    first commit

---
+++ assets/app.js
@@ -0,0 +1,32 @@
+// list out all possible variables 
+
+
+
+// what is the next steps? target button for on click listener
+    // on button click have timer start
+    
+
+
+
+// first question - What does HTML stand for?
+    // possible answers - HyperText Markup Language, HyperTalented Makeup Licensing, HelloTom Maybe Later, HyperText Markup Licensing
+    // incorrect answer will take away 15 seconds from time 
+    
+    
+// second queston - What does CSS stand for?
+    // Possible answers - Cascading Style Sheets, Cascade Skating Spot, Curious Scary Socks, Charlie Sierra Sierra
+    // incorrect answer will take away 15 seconds from time 
+    
+    
+// third question -  "JavaScript" has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.
+    // possible answers - Javascript, Jackson 5, HTML, CSS
+    // incorrect answer will take away 15 seconds from time 
+    
+    
+// fourth question - "UofA" has the best coding bootcamp!
+    // possible answers - UofA, Flatiron, DevMountain, Thinkful
+    // incorrect answer will take away 15 seconds from time 
+
+// final score 
+    // final score will be time remaining 
+    // allow user to input name to attach to score
